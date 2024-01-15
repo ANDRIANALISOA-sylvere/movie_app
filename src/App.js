39a9a17051759ce1b5eed63725ece98e81249c0b
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-01-16 00:07:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
+export default  function App()
+{
+    const [movies,setMovie]=useState([]);
+    useEffect(() => {
+        const fetchmovie = async()=>{
+            const res = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=7212e9c9a507d1f8bb208baf5ac276b5&page=1")
+            const data=await res.json()
+            setMovie(data.results);
+        }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        fetchmovie();
+    }, []);
+
+    console.log(movies);
+    const imagePath="https://image.tmdb.org/t/p/w500";
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                {movies.map((movie,index)=>
+                <div className="card mb-3 border-0 col-md-3">
+                        <img src={imagePath+movie.poster_path} alt={movie.title} key={movie.id} class="card-img-top" style={{height:'200px'}}/>
+                        <div className="card-body shadow">
+                            <div className="row">
+                                <div className="col-md-8">
+                                    <h5 className="card-title">{movie.title}</h5>
+                                </div>
+                                <div className="col">
+                                    <span className="badge rounded-pill text-bg-warning" style={{float:'right'}}>{movie.vote_average}</span>
+                                </div>
+                            </div>
+                            <small>Release date : {movie.release_date}</small>
+                        </div>
+                </div>
+                    )}
+            </div>
+            <ul>
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+
