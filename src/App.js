2e0de02dfd1f241f15b761866e3caaa9df8de8e3
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-01-19 00:43:47 +0300

    adding a btn description

--- src/App.js
+++ src/App.js
@@ -33,33 +33,56 @@ export default  function App()
             <div className="mb-3">
                 <input type="text" className="form-control" placeholder="Chercher un film ..." value={search} onChange={(e)=> setSearch(e.target.value)}/>
             </div>
-            <div className="row">
+            <div className="row justify-content-between">
                 {movies.length > 0 ? (
                     movies.map((movie,index)=>
-                            <div className="card mb-3 border-0 col-md-3">
-                                <img src={imagePath+movie.poster_path} alt={movie.title} key={movie.id} class="card-img-top" style={{height:'200px'}}/>
-                                <div className="card-body shadow">
-                                    <div className="row">
-                                        <div className="col-md-8">
-                                            <h5 className="card-title">{movie.title}</h5>
+                            <div  className="col-md-3 mt-5 div" key={movie.id}>
+                                <div className="card d-flex flex-column h-100">
+                                    <img src={imagePath+movie.poster_path} alt={movie.title} key={movie.id} className="card-img-top" style={{height:'200px'}}/>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-md-8">
+                                                <h5 className="card-title">{movie.title}</h5>
+                                            </div>
+                                            <div className="col">
+                                                <span className={movie.vote_average >=8 ? 'badge rounded-pill text-bg-success' : 'badge rounded-pill text-bg-warning'} style={{float:'right'}}>{movie.vote_average}</span>
+                                            </div>
                                         </div>
-                                        <div className="col">
-                                            <span className={movie.vote_average >=8 ? 'badge rounded-pill text-bg-success' : 'badge rounded-pill text-bg-warning'} style={{float:'right'}}>{movie.vote_average}</span>
+                                        <div className="row">
+                                            <div className="col-md-5">
+                                                <button type="button" className="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Description</button>
+                                            </div>
+                                            <div className="col-md-7">
+                                                <small>Date : {movie.release_date}</small>
+                                            </div>
+                                        </div>
+                                        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                            <div className="modal-dialog">
+                                                <div className="modal-content">
+                                                    <div className="modal-header">
+                                                        <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Understood</button>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <small>Release on : {movie.release_date}</small>
                                 </div>
                             </div>
                     )
-                ):(
-                    <div>
-                        <h1 className="text-center">Veuillez connecter à internet ....</h1>
-                    </div>
-                )}
-
+                    ):(
+                        <div>
+                            <h1 className="text-center">Chargement ....</h1>
+                        </div>
+                    )}
             </div>
-            <ul>
-            </ul>
         </div>
     )
 }
